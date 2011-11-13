d6994055e36f77e5b416578af61ed9a6d4631153
Robert Scholte <rfscholte@apache.org>
2011-11-13 10:02:14 +0000

    Fix/add licenses
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/jdiff-maven-plugin@15105 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/apache/maven/plugin/jdiff/JDiffUtils.java
+++ src/main/java/org/apache/maven/plugin/jdiff/JDiffUtils.java
@@ -1,5 +1,24 @@
 package org.apache.maven.plugin.jdiff;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,7 +29,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.reporting.MavenReportException;
 import org.codehaus.plexus.util.FileUtils;
@@ -21,14 +39,13 @@ public class JDiffUtils
     {
         // hide constructor of utility class
     }
-    
+
     public static List<String> getProjectSourceRoots( MavenProject p )
     {
         if ( "pom".equals( p.getPackaging().toLowerCase() ) )
         {
             return Collections.emptyList();
         }
-
         return ( p.getCompileSourceRoots() == null ? Collections.EMPTY_LIST
                         : new LinkedList( p.getCompileSourceRoots() ) );
     }
@@ -42,19 +59,20 @@ public class JDiffUtils
         {
             classpathElements.add( a.getFile().getPath() );
         }
-        
         return classpathElements;
     }
-    
-    public static Set<String> getPackages( MavenProject project ) 
+
+    public static Set<String> getPackages( MavenProject project )
     {
         Set<String> packages = new HashSet<String>();
-        List<String> compileRoots = project.getCompileSourceRoots(); 
-        for ( String compileRoot : compileRoots  )
+        List<String> compileRoots = project.getCompileSourceRoots();
+        for ( String compileRoot : compileRoots )
         {
             try
             {
-                List<String> files = FileUtils.getFileNames(  FileUtils.resolveFile( project.getBasedir(), compileRoot), "**/*.java", null, false );
+                List<String> files =
+                    FileUtils.getFileNames( FileUtils.resolveFile( project.getBasedir(), compileRoot ), "**/*.java",
+                                            null, false );
                 for ( String file : files )
                 {
                     packages.add( FileUtils.dirname( file ).replace( File.separatorChar, '.' ) );
@@ -62,11 +80,9 @@ public class JDiffUtils
             }
             catch ( IOException e )
             {
-                //do nothing
+                // do nothing
             }
         }
-        
         return packages;
-            
     }
 }
